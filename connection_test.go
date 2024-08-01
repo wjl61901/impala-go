ae0e0bf24cab5b3d956086085e2c25b137c4910a
Marin Nozhchev <murfffi@gmail.com>
2024-08-01 16:23:28 +0300

    Run integration tests in CI (#2)

--- connection_test.go
+++ connection_test.go
@@ -1,31 +1,48 @@
 package impala
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"github.com/sclgo/impala/internal/fi"
+	"github.com/stretchr/testify/require"
+	"net"
+	"net/url"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestPinger(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skip long tests")
+func TestIntegration(t *testing.T) {
+	fi.SkipLongTest(t)
+
+	dsn := os.Getenv("IMPALA_DSN")
+	if dsn == "" {
+		ctx := context.Background()
+		c := fi.NoError(Setup(ctx)).Require(t)
+		defer fi.NoErrorF(fi.Bind(c.Terminate, ctx), t)
+		dsn = GetDsn(ctx, t, c)
 	}
 
-	conn := open(t)
+	conn := open(t, dsn)
 	defer conn.Close()
-	err := conn.Ping()
-	if err != nil {
-		t.Errorf("Failed to hit database")
-	}
+
+	t.Run("Pinger", func(t *testing.T) {
+		testPinger(t, conn)
+	})
+	t.Run("Select", func(t *testing.T) {
+		testSelect(t, conn)
+	})
 }
 
-func TestSelect(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skip long tests")
-	}
+func testPinger(t *testing.T, conn *sql.DB) {
+	require.NoError(t, conn.Ping())
+}
 
+func testSelect(t *testing.T, db *sql.DB) {
 	sampletime, _ := time.Parse(time.RFC3339, "2019-01-01T12:00:00Z")
 
 	tests := []struct {
@@ -46,31 +63,47 @@ func TestSelect(t *testing.T) {
 	}
 
 	var res interface{}
-	db := open(t)
-	defer db.Close()
 
 	for _, tt := range tests {
 		t.Run(tt.sql, func(t *testing.T) {
 			err := db.QueryRow(fmt.Sprintf("select %s", tt.sql)).Scan(&res)
-			if err != nil {
-				t.Error(err)
-			}
-
-			if res != tt.res {
-				t.Errorf("got: %v (%T), want: %v (%T)", res, res, tt.res, tt.res)
-			}
+			require.NoError(t, err)
+			require.Equal(t, res, tt.res)
 		})
 	}
 }
 
-func open(t *testing.T) *sql.DB {
-	dsn := os.Getenv("IMPALA_DSN")
-	if dsn == "" {
-		t.Skip("No connection string")
-	}
+func open(t *testing.T, dsn string) *sql.DB {
 	db, err := sql.Open("impala", dsn)
 	if err != nil {
 		t.Fatalf("Could not connect to %s: %s", dsn, err)
 	}
 	return db
+}
+
+const dbPort = "21050/tcp"
+
+func Setup(ctx context.Context) (testcontainers.Container, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "apache/kudu:impala-latest",
+		ExposedPorts: []string{dbPort},
+		Cmd:          []string{"impala"},
+		WaitingFor:   wait.ForLog("Impala has started.").WithStartupTimeout(3 * time.Minute),
+	}
+	return testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+}
+
+func GetDsn(ctx context.Context, t *testing.T, c testcontainers.Container) string {
+	port := fi.NoError(c.MappedPort(ctx, dbPort)).Require(t).Port()
+	host := fi.NoError(c.Host(ctx)).Require(t)
+	u := &url.URL{
+		Scheme: "impala",
+		Host:   net.JoinHostPort(host, port),
+		User:   url.User("impala"),
+	}
+	t.Log("url", u.String())
+	return u.String()
 }
